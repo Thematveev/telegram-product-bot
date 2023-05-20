33bf1d4e28421b19e652f045cd907371df5be2a0
Matveev Maksim <thematveev21@gmail.com>
2023-05-20 13:57:06 +0300

    add db features

---
+++ db.py
@@ -0,0 +1,17 @@
+from peewee import SqliteDatabase, CharField, IntegerField, Model
+
+connection = SqliteDatabase('history.db')
+
+
+class Query(Model):
+    chat_id = CharField(max_length=100, null=False)
+    type = CharField(max_length=20, null=False)
+    category = CharField(max_length=100)
+    amount = IntegerField()
+
+    class Meta:
+        database = connection
+        db_table = 'queries'
+
+
+Query.create_table()
